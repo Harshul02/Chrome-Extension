18e0a83d677027535e352abff2f174baba2c23bd
Harshul Jain <harshulgangwal02@gmail.com>
2022-05-08 16:11:29 +0530

    Added Delete Function

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 let myLeads=[];
+let oldLeads=[];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEL = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(leadsFromLocalStorage)
 {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value);
-    inputEl.value="";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
-})
-
-
-function renderLeads()
+function render(leads)
 {
     let listItems="";
     
-    for(let i=0; i<myLeads.length; i++)
+    for(let i=0; i<leads.length; i++)
     {
         listItems +="<li><a target='_blank' href='" + myLeads[i] + "'>" +myLeads[i] + "</a></li>";
     }
 
     ulEL.innerHTML=listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads=[];
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value="";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+})
+
+
